019bbce440aff2ed2432cda6bbf2a363c53e4e4d
João Henrique Silva <40443481+iamjoaohenrique@users.noreply.github.com>
2020-08-04 22:55:34 -0300

    criado o estilo css global da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import './assets/styles/global.css';
 
+//JSX - Javascript + XML
 
 function App() {
   return (
     <div className="App">
-     <h1>hello word</h1>
+      <h1>hello word</h1>
+      <button type='button'>clica aqui</button>
     </div>
   );
 }
